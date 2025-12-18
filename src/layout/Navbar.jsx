18939e060385f363307459843f59bf257df470bc
Sharon <naisiayee@gmail.com>
2025-12-18 12:12:42 -0500

    worked in app,navbar,layout

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,23 +1,35 @@
-import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+import { NavLink } from "react-router";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
-  const { token, logout } = useAuth();
-  const { setPage } = usePage();
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
-        {token ? (
-          <a onClick={() => logout()}>Log out</a>
-        ) : (
-          <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
-          </>
-        )}
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Activities
+        </NavLink>
+        <NavLink
+          to="/logout"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Log Out
+        </NavLink>
+        <NavLink
+          to="/register"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Register
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Login
+        </NavLink>
       </nav>
     </header>
   );
